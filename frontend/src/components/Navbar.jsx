899c6ad1a8bdc69335b31c3165207c8e6bc58b66
Salih <salihmizipow@mail.ru>
2026-03-27 23:22:55 +0300

    фронт

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,8 +13,8 @@ function ProfilePersonIcon({ className }) {
   return (
     <svg
       className={className}
-      width="22"
-      height="22"
+      width="18"
+      height="18"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
